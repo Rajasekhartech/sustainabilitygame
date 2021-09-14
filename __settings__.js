07e8e3485924700c807732c27f377dba8185bdbd
Rajasekhar <rajasekhartechpy@gmail.com>
2021-09-15 01:11:19 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1224867.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 54906582, 55194416, 55201104, 55206352, 55206353, 55206354, 55206355, 55206358, 55206400, 55256609, 55257428, 55269142, 55272370, 55280071, 55280072, 55280073, 55280970 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/55196188/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/55196189/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/55196187/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/55139573/1/basis.wasm.js', 'wasmUrl' : 'files/assets/55139574/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/55139572/1/basis.js', 'preload' : false},
+];
